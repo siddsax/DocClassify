489aaf773ddcf50853bd5249cac5be33410ca5ab
Siddhartha Saxena <ss.siddharthasaxena@gmail.com>
2018-10-29 05:19:20 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,14 +1,27 @@
 import numpy as np
+import argparse
+
 from classifiers import classifiers
 from representations import representations
 
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+parser.add_argument('--N', dest='N', type=int, default=0, help='number of samples used for training. Default=0')
+parser.add_argument('--C', dest='C', type=int, default=1, help='Classifier. 1=logistic, 2=Naive Bayes, 3=SVM')
+args = parser.parse_args()
 
 reps = representations()
-data = reps.tfidf(n=10)
 
+if args.N != 0:
+  data = reps.tfidf(n=args.N)
+else:
+  data = reps.tfidf(n)
 
 classifier = classifiers(data[0], data[1])
-print(classifier.classify_lggistic())
-print(classifier.naive_bayes())
-print(classifier.SVM())
-# print(classifier.FFNet())
+
+if args.C == 1:
+  print(classifier.classify_lggistic())
+elif args.C == 2:
+  print(classifier.naive_bayes())
+else:
+  print(classifier.SVM())
